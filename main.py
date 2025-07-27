a711816b371e70af5ca10cf38aa0d0c48338d61b
dominikvicha <161117975+dominikvicha@users.noreply.github.com>
2025-07-27 10:00:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,42 @@ from mysql.connector import Error
 
 def connection_db():
     try:
-        conn = mysql.connectorconnector.connect(
+        conn = mysql.connector.connect(
             host = "localhost",
             user = "root",
             password = "1111",
-            database = "Testing_academy_projekt2"
+            database = "TA_project2"
         )
-
         print("Připojení bylo úspěšné.")
         return conn
     
     except mysql.connector.Error as  err:
         print(f"Chyba při připojování: {err}")
         return None
+    
+
+def vytvoreni_tabulky(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE ukoly (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nazev VARCHAR(50),
+                popis TEXT,
+                stav ENUM('nezahájeno', 'hotovo', 'probíhá'),
+                datum_vytvoreni DATE
+            )
+        ''')
+        print("Tabulka byla vytvořena.")
+
+    except Error as err:
+        print(f"Tabulka nebyla vytvořena. {err}")
+
+
+
+
+if __name__ == "__main__":
+    conn = connection_db()
+    if conn:
+        vytvoreni_tabulky(conn)
+        conn.close()
